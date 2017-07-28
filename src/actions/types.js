2fd56fcd0edc59c1b4fed393e6eea38ae37f1535
namkwon <namjookwon76@gmail.com>
2017-07-27 22:47:19 -0400

    added logout feature

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,5 @@
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
 export const LOGIN_FAIL = 'LOGIN_FAIL'
+export const LOGOUT = 'LOGOUT'
 export const GET_USER_INFO_SUCCESS = 'GET_USER_INFO_SUCCESS'
 export const GET_USER_INFO_FAIL = 'GET_USER_INFO_FAIL'
